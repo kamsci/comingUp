a1498ed333e83ee3c2b7edaa7dd6a07f33c1fc56
Krista Calderon <kamsci@gmail.com>
2016-07-13 16:42:47 -0700

    added validations to student model and relationship to user model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,21 @@
 class Student < ActiveRecord::Base
   belongs_to :user
+
+  has_secure_password
+
+  validates :brand,
+    length {in: 10..600}
+
+  validates :linkedin,
+    :url => true
+
+  validates :resume,
+    :url => true
+
+  validates :jobtracker,
+    :url => true
+
+  validates :portfolio,
+    :url => true
+
 end
