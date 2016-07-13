3481c97204f80ec13a245f301a9af3b4abffaf93
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-07-13 16:32:29 -0700

    added students controller

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,19 @@
+class StudentsController < ApplicationController
+  def index
+    
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  # def student_params
+  #   params.require(:student).permit(:user_id, )
+  # end
+end
