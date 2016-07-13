44e7f57f02a3c7020ae092886fb7f576272340cf
Krista Calderon <kamsci@gmail.com>
2016-07-13 14:19:54 -0700

    added session create and destory methods add reference to user authenticate def; created flash partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,30 @@
 class SessionsController < ApplicationController
+
+  user = User.authenitcate(user_params)
+
   def new
+  end
+
+  def create
+    if user
+      session[:user_id] = user.id
+      flash[:success] = 'You are logged in'
+      redirect_to root_path
+    else
+      flash[:danger] = 'Credentials invalid, please try again'
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You are logged out'
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
