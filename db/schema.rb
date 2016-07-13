415016f1a80980561ae26246ec0d09dcc2302774
Krista Calderon <kamsci@gmail.com>
2016-07-13 16:33:51 -0700

    added students model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713204324) do
+ActiveRecord::Schema.define(version: 20160713233132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "students", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "brand"
+    t.text     "linkedin"
+    t.text     "resume"
+    t.text     "jobtracker"
+    t.text     "portfolio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "students", ["user_id"], name: "index_students_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -25,4 +38,5 @@ ActiveRecord::Schema.define(version: 20160713204324) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "students", "users"
 end
