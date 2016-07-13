8a91ca15e48e01c43b0840a6f8ebdfd11be9b20c
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-07-13 16:57:21 -0700

    students#index page working

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,20 @@
 class StudentsController < ApplicationController
   def index
-    
+    # @students = Student.all
+    @users = User.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    Student.find(params[:id]).update(student_params)
+    redirect_to student_path(params[:id])
   end
 
   private
