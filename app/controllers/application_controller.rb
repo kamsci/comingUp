b2b938307ac659bd28a8153eae5a2befb231ba2d
Krista Calderon <kamsci@gmail.com>
2016-07-13 16:20:15 -0700

    added basic nav links: login, logout, new user; fixed authenitate bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :current_user
+
   def is_authenticated
     unless current_user
       flash[:danger] = "Credentials Invalid"
@@ -13,4 +15,5 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
+  
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.authenitcate(user_params)
+    user = User.authenticate(user_params)
     if user
       session[:user_id] = user.id
       flash[:success] = 'You are logged in'
