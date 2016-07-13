d607d449346d9d3811910977809557f421547432
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-07-13 14:16:35 -0700

    user routes set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,40 @@
 class UsersController < ApplicationController
-  def show
+  # show users profile
+  def show  
+    @user = User.find_by_id(params[:id])
   end
 
+  # create a new blank template for user, direct to new user page
   def new
+    @user = User.new
   end
 
-  def edit
+  # create a new user in the db, redirect to admin profile
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path
+    else
+      flash[:danger] = user.errors.messages
+      redirect_to new_user_path
+    end
   end
+
+  # pull user info, populate form, pull up form
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # update already existing user
+  def update
+    User.find(params[:id]).update(user_params)
+    redirect_to user_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
