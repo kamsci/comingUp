b2e88edd58820d79d3f52622a135d4e7f6e85d8a
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-07-13 13:48:18 -0700

    made user model

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+
+  validates :name,
+    presence: true,
+    length: {in: 1..30}
+
+  validates :email,
+    presence: true,
+    email: true,
+    uniqueness: {case_sensitive: false}
+
+  validates :password,
+    length: {in: 8..72},
+    on: :create
+
+  def self.authenticate(params)
+    User.find_by_email(params[:email]).try(:authenticate, params[:password])
+  end
+  
+end
