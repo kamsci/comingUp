cd5717dbe7880eaa9ea94397e1e9bf9f079863d2
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-07-14 22:25:22 -0700

    js starting to work for profile page

---
+++ app/assets/javascripts/students.js
@@ -0,0 +1,13 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function() {
+  console.log('edit ready');
+
+  $('.edit').on('click', function(e){
+    console.log('I\'ve been clicked!');
+    $('.update').removeAttr('readonly');
+    $('.submit').
+  });
+});
