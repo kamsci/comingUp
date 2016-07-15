7a7bea518daf98a1a2973bf5844129bbe2a81f62
Joe Marzullo <jmarzu13@gmail.com>
2016-07-14 23:27:44 -0700

    All forms have bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      @student = user.students.create()
+      @students = user.students.create()
       redirect_to students_path
     else
       flash[:danger] = user.errors.messages
