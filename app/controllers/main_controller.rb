c4dfd6288abd4b5ca928446e8ca516c96c9c91ad
Joe Marzullo <jmarzu13@gmail.com>
2016-07-15 13:16:22 -0700

    API search works

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,12 +4,13 @@ class MainController < ApplicationController
     zip = '98101'
     country = 'USA'
     city = 'Seattle'
-    params = 'Web'
-    @topic = params.to_s
+    @topic = params[':topic']
+    if !@topic
+      @topic = 'Web'
+    end
 
     data = RestClient.get 'https://api.meetup.com/2/open_events?sign=true&photo-host=public&zip=' + zip + '&country=' + country + '&topic=' + @topic + '&city=' + city + '&state=WA&page=20&key=23145310778c71694fbb51774f'
     @data = JSON.parse(data)
-    puts '---------------------', @topic
   end
 
 end
