2e372d04ca247e1087813e61da15c7660f3b113b
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-07-17 17:16:15 -0700

    have all the comment forms set up, now need to have them show under appropriate thing to comment on

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,6 +12,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.create(review_params)
     render json: @review
+    # redirect_to student_path(params)
   end
 
   def edit
