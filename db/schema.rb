f89b442ceba6cbe7f6fda667e7f12e7ddf3cd118
Krista Calderon <kamsci@gmail.com>
2016-07-18 19:34:15 -0700

    saved schema after upstream pull

--- db/schema.rb
+++ db/schema.rb
@@ -32,13 +32,37 @@ ActiveRecord::Schema.define(version: 20160718220929) do
   add_index "cohorts_students", ["cohort_id"], name: "index_cohorts_students_on_cohort_id", using: :btree
   add_index "cohorts_students", ["student_id"], name: "index_cohorts_students_on_student_id", using: :btree
 
+  create_table "deliverables", force: :cascade do |t|
+    t.string   "deliverable_type"
+    t.string   "deliverable_name"
+    t.date     "due_date"
+    t.text     "notes"
+    t.integer  "cohort_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "deliverables", ["cohort_id"], name: "index_deliverables_on_cohort_id", using: :btree
+
+  create_table "githubstudents", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider_id"
+    t.string   "provider_token"
+    t.string   "provider_name"
+    t.text     "picture"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "githubstudents", ["user_id"], name: "index_githubstudents_on_user_id", using: :btree
+
   create_table "reviews", force: :cascade do |t|
     t.integer  "student_id"
     t.string   "review_type"
-    t.string   "reviewer_name"
-    t.text     "review_content"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "reviewer_name"
+    t.text     "review_content"
   end
 
   add_index "reviews", ["student_id"], name: "index_reviews_on_student_id", using: :btree
@@ -70,14 +94,12 @@ ActiveRecord::Schema.define(version: 20160718220929) do
     t.boolean  "admin"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "provider_id"
-    t.string   "provider_token"
-    t.string   "provider_name"
-    t.text     "picture"
   end
 
   add_foreign_key "cohorts_students", "cohorts"
   add_foreign_key "cohorts_students", "students"
+  add_foreign_key "deliverables", "cohorts"
+  add_foreign_key "githubstudents", "users"
   add_foreign_key "reviews", "students"
   add_foreign_key "students", "users"
 end
