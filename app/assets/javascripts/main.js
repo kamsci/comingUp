fe4edea196a80f8370ca53c7e41762c30e5636fc
Krista Calderon <kamsci@gmail.com>
2016-07-18 22:52:00 -0700

    updated flash message to time out and be absolute to the body container

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,11 +2,19 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 
+$(document).ready(function() {
 
-$('#myTabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
+  $('#myTabs a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  });
+
+  $(function() {
+   $('#flash').delay(500).fadeIn('normal', function() {
+      $(this).delay(1500).fadeOut();
+   });
 });
 
-
 console.log('the main.js page');
+
+}); //end doc ready
