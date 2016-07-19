d282571fa313c8c31a6787a3bc72b85c97a28760
Joe Marzullo <jmarzu13@gmail.com>
2016-07-19 14:59:25 -0700

    ActionMailer Works on development

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :cohorts, through: :student
   has_many :reviews, through: :student
   has_many :deliverables, through: :cohorts
+
+  #attr_accessor :name, :email
   
   accepts_nested_attributes_for :student
 
@@ -26,4 +28,13 @@ class User < ActiveRecord::Base
     User.find_by_email(params[:email]).try(:authenticate, params[:password])
   end
 
+  def set_password_reset
+    self.reset_code = loop do
+      code = SecureRandom.urlsafe_base64
+      break code unless User.exists?(reset_code: code)
+    end
+    self.expires_at = 4.hours.from_now
+    self.save
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718220929) do
+ActiveRecord::Schema.define(version: 20160719165542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 20160718220929) do
     t.string   "provider_token"
     t.string   "provider_name"
     t.text     "picture"
+    t.string   "reset_code"
+    t.datetime "expires_at"
   end
 
   add_foreign_key "cohorts_students", "cohorts"
