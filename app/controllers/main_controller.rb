ce537aac507b3c6fc695c5bb23093847b8473bb7
Joe Marzullo <jmarzu13@gmail.com>
2016-07-18 20:52:47 -0700

    Perfecting the api error code

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -11,9 +11,11 @@ class MainController < ApplicationController
       @topic = params[':topic'].gsub(/\s+/, "")
     end
 
-    data = RestClient.get 'https://api.meetup.com/2/open_events?sign=true&photo-host=public&zip=' + zip + '&country=' + country + '&topic=' + @topic + '&city=' + city + '&state=WA&page=20&key=23145310778c71694fbb51774f'
+      data = RestClient.get 'https://api.meetup.com/2/open_events?sign=true&photo-host=public&zip=' + zip + '&country=' + country + '&topic=' + @topic + '&city=' + city + '&state=WA&page=20&key=23145310778c71694fbb51774f'
+      
+      puts '123123123', data
 
-    if data['results'] == [] 
+    if data['results'] == [] || data['code'] == '400 Bad Request' 
       flash[:danger] = 'No Search Results, please try again'
     else
       @data = JSON.parse(data)
