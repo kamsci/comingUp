6d5f0025bde19722593d7b4213c1472ac6bed3bd
Krista Calderon <kamsci@gmail.com>
2016-09-09 15:37:11 -0700

    refactored main_ctrl to only show cohorts and deliverables that match the logged in student. TODO: troubleshoot @students results

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -26,18 +26,37 @@ class MainController < ApplicationController
     end
 
     @picks = Staffpick.all
-    @cohorts = Cohort.all
-    @users = User.all
-    @cohorts = Cohort.all
+    # @cohorts = Cohort.all
+    # @users = User.all
 
-    if (params[:cohort] && params[:cohort][:cohort] && params[:cohort][:cohort] != '')
-      @students = User.joins(:student, :cohorts).select('*').where(cohorts:{cohort: params[:cohort][:cohort]})
-      @deliverables = Deliverable.select('*').joins(:cohort).where(cohorts:{cohort: params[:cohort][:cohort]})
-    else
+    # if (params[:cohort] && params[:cohort][:cohort] && params[:cohort][:cohort] != '')
+    #   @students = User.joins(:student, :cohorts).select('*').where(cohorts:{cohort: params[:cohort][:cohort]})
+    #   @deliverables = Deliverable.select('*').joins(:cohort).where(cohorts:{cohort: params[:cohort][:cohort]})
+    # else
+    #   @students = User.joins(:student, :cohorts).select('*')
+    #   @deliverables = Deliverable.joins(:cohort).select('*')
+    # end
+
+
+    if (@current_user.admin == false)
+      @students = User.select('*').joins(:student, :cohorts)
+      # .where(cohort_id: user_cohort)
+      @deliverables = Deliverable.select('*').joins(:cohort).where(cohort_id: user_cohort)
+      @cohorts = Cohort.select('*').where(id: user_cohort)
+    elsif @current_user.admin == true
       @students = User.joins(:student, :cohorts).select('*')
       @deliverables = Deliverable.joins(:cohort).select('*')
-    end
+      @cohorts = Cohort.all
+    end 
 
+  end
+
+  private
+
+  def user_cohort
+    current_student = Student.where(user_id: @current_user.id).pluck(:id)
+    expected_cohort = CohortsStudents.where(student_id: current_student).pluck(:cohort_id)
+    return expected_cohort
   end
 
 end
--- spec/controllers/cohorts_controller_spec.rb
+++ spec/controllers/cohorts_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CohortsController, :type => :controller do
   describe 'GET #index' do
     it ' responds successfully with a 302 status code' do
       get :index
-      expect(response).to be_success
+      expect(page).to have_http_status(:success)
       expect(response).to have_http_status(302)
     end
 
@@ -19,16 +19,19 @@ RSpec.describe CohortsController, :type => :controller do
       # expect(assigns(@cohorts)).to match_array([cohort1, cohort2])
       expect(Cohort.all.order('cohort ASC')).to eq([id: 1, cohort: "WDItest", created_at: "2016-09-09 18:36:07", updated_at: "2016-09-09 18:36:07", id: 2, cohort: "WDItestUI", created_at: "2016-09-09 18:36:07", updated_at: "2016-09-09 18:36:07", id: 1, cohort: 'WDItest', created_at: "2016-09-09 18:36:07", updated_at: "2016-09-09 18:36:07"])
     end
-    xit 'INDEX creates an @student obj combining user, student, and cohort tables' do
+
+    xit 'creates @student obj combining user, student, and cohort tables' do
+      get :index
     end
 
-    xit 'INDEX creates an @deliverables obj combining deliverable and cohort tables' do
+    xit 'creates an @deliverables obj combining deliverable and cohort tables' do
     end
   end
 
   describe 'POST #create' do
     it 'CREATE creates a new cohort' do
       cohort1 = Cohort.create!(cohort: 'WDItest')
+      # expect(cohort1).to be_a_new(Cohort)
       expect(cohort1.cohort).to eq('WDItest')
     end
   end
